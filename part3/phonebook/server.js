7f65b6d2acd8fe259e1f7ce5a7cb718f2db5a95b
kritebh <kritebhlaganbibhakar@gmail.com>
2023-02-15 22:06:17 +0530

    3.1: Phonebook backend step1

---
+++ part3/phonebook/server.js
@@ -0,0 +1,35 @@
+const http = require('http')
+
+const data = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const app = http.createServer((req,res)=>{
+       res.writeHead(200,{ 'Content-Type': 'text/plain' })
+       res.end(JSON.stringify(data)) 
+})
+
+const PORT = 3001
+app.listen(PORT)
+
+console.log(`Server is started on port ${PORT}`)
+
