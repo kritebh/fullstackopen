85a67ba3c893493f7c93d111fe8699270c695f13
kritebh <kritebhlaganbibhakar@gmail.com>
2023-02-16 11:14:39 +0530

    3.7: Phonebook backend step7

--- part3/phonebook/server.js
+++ part3/phonebook/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
+//Middleware
 app.use(express.json())
+app.use(morgan('tiny'))
+
+
 let data = [
     { 
       "id": 1,
